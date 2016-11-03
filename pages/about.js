99cce08ffcd933b3660ae92711eb51b453e45423
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-11-03 23:50:00 +0800

    [#0] - Initial next.js project.

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <h1>Hello, I'm mobile-ready!</h1>
+  </div>
+)
